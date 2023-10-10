483ebd528678fc79bcc7798595e0385c31b42721
David Ghulijanyan <david.hayki.ghulijanyan@gmail.com>
2023-10-10 22:52:58 +0400

    feat: add Logo

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,7 +1,10 @@
+import Image from 'next/image';
 import styles from './logo.module.scss';
 
-function Logo() {
-	return <div className={styles.root}>Logo</div>
+function Logo({ src = "/assets/images/logo.webp" }: { src: string} ) {
+	return <div className={styles.root}>
+		<Image src={src} alt="logo" width={208} height={104} />
+	</div>
 }
 
 export { Logo };

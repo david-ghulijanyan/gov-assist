351285279ac9448de3f82095edd267981b75d5ca
David Ghulijanyan <david.hayki.ghulijanyan@gmail.com>
2023-10-10 21:58:19 +0400

    feat: add datas

---
+++ src/data/index.ts
@@ -0,0 +1,10 @@
+import codes from "./codes.json";
+import fees from "./fees.json";
+import formCheck from "./formCheck.json";
+import languages from "./languages.json";
+import list from "./list.json";
+import navigations from "./navigations.json";
+import quotes from "./quotes.json";
+import reviews from "./reviews.json";
+
+export { codes, fees, formCheck, languages, list, navigations, quotes, reviews };

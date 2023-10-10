d9c252ccdcb06cded0eed4c03019b33bc26a3e72
David Ghulijanyan <david.hayki.ghulijanyan@gmail.com>
2023-10-10 22:00:08 +0400

    feat: add utils

---
+++ src/utils/replaceWithJsx.tsx
@@ -0,0 +1,30 @@
+import { ComponentType, createElement } from "react";
+
+
+interface TagProps {
+    tag: keyof JSX.IntrinsicElements | ComponentType<any>;
+    id: string;
+    props?: Record<string, any>;
+}
+
+function replaceWithJSX(input: string, tags: TagProps[]) {
+    const splitByPattern = input.split(/%\w+%/g);
+    const matches = Array.from(input.matchAll(/%(\w+)%/g));
+
+    const interleaved: (string | JSX.Element)[] = [];
+    splitByPattern.forEach((part, idx) => {
+        interleaved.push(part);
+        if (matches[idx]) {
+            const tagInfo = tags.find(tag => tag.id === matches[idx][1]);
+            if (tagInfo) {
+                const Element = tagInfo.tag;
+                const propsWithChildren = { ...tagInfo.props, children: matches[idx][1], key: idx };
+                interleaved.push(createElement(Element, propsWithChildren as any));
+            }
+        }
+    });
+
+    return interleaved;
+}
+
+export default replaceWithJSX;

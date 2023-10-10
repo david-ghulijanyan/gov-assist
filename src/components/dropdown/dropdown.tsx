1bb56877f5fc8ee7c9f4082ddcd95b269ead5396
David Ghulijanyan <david.hayki.ghulijanyan@gmail.com>
2023-10-10 22:59:52 +0400

    feat: add dropdown

--- src/components/dropdown/dropdown.tsx
+++ src/components/dropdown/dropdown.tsx
@@ -1,7 +1,55 @@
+import { FC, useRef, useState } from 'react';
 import styles from './dropdown.module.scss';
 
-function Dropdown() {
-	return <div className={styles.root}>Dropdown</div>
+interface DropdownProps {
+  options: string[];
+  label?: string;
+  onChange: (value: string) => void;
 }
 
-export { Dropdown };
+const Dropdown: FC<DropdownProps> = ({ options, label, onChange }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState<string | undefined>();
+	const ref = useRef<HTMLDivElement | null>(null);
+	
+  const handleOutsideClick = (event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      setIsOpen(false);
+      document.removeEventListener('mousedown', handleOutsideClick);
+    }
+  };
+
+  const handleSelection = (option: string) => {
+    setSelected(option);
+    onChange(option);
+    setIsOpen(false);
+  };
+
+  const toggleDropdown = () => {
+    if (!isOpen) {
+      document.addEventListener('mousedown', handleOutsideClick);
+    } else {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    }
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={styles.root} ref={ref}>
+      <button className={styles.button} onClick={toggleDropdown}>
+        {selected || label || 'Select option'}
+      </button>
+      {isOpen && (
+        <ul className={styles.list}>
+          {options.map(option => (
+            <li key={option} onClick={() => handleSelection(option)}>
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;

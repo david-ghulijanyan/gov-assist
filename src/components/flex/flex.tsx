e61172cee99097f690578f77c7088b604cf850cf
David Ghulijanyan <david.hayki.ghulijanyan@gmail.com>
2023-10-10 23:25:48 +0400

    feat: add Flex

--- src/components/flex/flex.tsx
+++ src/components/flex/flex.tsx
@@ -1,8 +1,42 @@
-import { FC, PropsWithChildren } from 'react';
+// Flex.tsx
+import { CSSProperties, PropsWithChildren } from 'react';
 import styles from './flex.module.scss';
 
-const Flex: FC<PropsWithChildren> = ({children}) => {
-	return <div className={styles.root}>{children}</div>
+interface BaseFlexProps {
+  direction?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
+  wrap?: 'nowrap' | 'wrap' | 'wrap-reverse';
+  justifyContent?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly';
+  alignItems?: 'flex-start' | 'flex-end' | 'center' | 'baseline' | 'stretch';
+  alignContent?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'stretch';
+  style?: CSSProperties;
 }
+
+type FlexProps = PropsWithChildren<BaseFlexProps>;
+
+const Flex = ({
+  direction = 'row',
+  wrap = 'nowrap',
+  justifyContent = 'flex-start',
+  alignItems = 'stretch',
+  alignContent = 'stretch',
+  children,
+  style,
+}: FlexProps) => {
+  return (
+    <div
+      className={styles.root}
+      style={{
+        flexDirection: direction,
+        flexWrap: wrap,
+        justifyContent: justifyContent,
+        alignItems: alignItems,
+        alignContent: alignContent,
+        ...style,
+      }}
+    >
+      {children}
+    </div>
+  );
+};
 
 export { Flex };
